94591f526d1841d75f7bb63d5d989edd3124b4ac
Jonathan <steelsouljs@gmail.com>
2022-06-01 08:56:20 -0700

    Fourth Commit: added gameplay screens up to screen 8, just need screens 9 and 10 to start working on the actual program code.

---
+++ app/src/main/java/com/example/annoyingmazegame/EighthActivity.java
@@ -0,0 +1,14 @@
+package com.example.annoyingmazegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EighthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eighth);
+    }
+}
