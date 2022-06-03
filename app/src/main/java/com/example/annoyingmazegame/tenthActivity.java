939ff0fc6d8c04ff39261c076ee346f9a5eeeb2b
Jonathan <steelsouljs@gmail.com>
2022-06-03 08:52:15 -0700

    sixth commit: finished my tenth and final screen and will begin the navigational code as well as the intent variables keeping between screens.

---
+++ app/src/main/java/com/example/annoyingmazegame/tenthActivity.java
@@ -0,0 +1,14 @@
+package com.example.annoyingmazegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tenthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenth);
+    }
+}
