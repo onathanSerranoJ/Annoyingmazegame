fa7b126ef6e008efc1d7f0a5e3a491ef2edcf832
Jonathan <steelsouljs@gmail.com>
2022-06-03 08:48:52 -0700

    fifth commit: finished my ninth screen and will finish my tenth screen

---
+++ app/src/main/java/com/example/annoyingmazegame/NinthActivity.java
@@ -0,0 +1,14 @@
+package com.example.annoyingmazegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NinthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ninth);
+    }
+}
